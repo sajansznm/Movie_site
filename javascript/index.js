cd5af558a7149471cc82e5589515f5aec9791dfc
sajansznm <sajan@virtual-gravity.com>
2023-05-22 21:05:53 +0545

    Added the Javascript Toogle Menu Functionality

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,33 @@
 **/
 
 
+/**
+ * nabvar 
+ */
+
+
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+
+const navbar = document.querySelector("[data-navbar]");
+
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArray = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArray.length; i++) {
+
+  navElemArray[i].addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+  });
+};
+
 
 
 
